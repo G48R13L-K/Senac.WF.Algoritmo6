7cb0aa38df0e6354c96bd4f16ca9e474069880e3
GABRIELAUGUSTOKAPPENBERG <kappenberg4@gmail.com>
2025-04-08 21:54:36 -0300

    Criação aplicativo Windows form, alteração das propriedades graficas do aplicativo.

---
+++ Algoritmo6/Algoritmo6/Form1.cs
@@ -0,0 +1,15 @@
+namespace Algoritmo6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
